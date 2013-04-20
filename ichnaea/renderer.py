bb5e2ae68d8a670ea4f988868bb34214e595a7ee
Hanno Schlichting <hanno@hannosch.eu>
2013-04-20 10:22:04 +0200

    factor out dump_decimal_json function

--- ichnaea/renderer.py
+++ ichnaea/renderer.py
@@ -3,15 +3,18 @@ import decimal
 import simplejson as json
 
 
+def dump_decimal_json(value):
+    with decimal.localcontext() as ctx:
+        ctx.prec = 6
+        return json.dumps(value, use_decimal=True)
+
+
 class DecimalJSON(object):
 
     def __call__(self, info):
         def _render(value, system):
-            with decimal.localcontext() as ctx:
-                ctx.prec = 6
-                ret = json.dumps(value, use_decimal=True)
             request = system.get('request')
             if request is not None:
                 request.response.content_type = 'application/json'
-            return ret
+            return dump_decimal_json(value)
         return _render
