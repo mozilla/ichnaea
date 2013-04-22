bd5e764ef5ec6e2f3f0807f9c29ea8a5a966f154
Hanno Schlichting <hanno@hannosch.eu>
2013-04-22 15:28:19 +0200

    avoid tons of code in packages `__init__.py`

--- ichnaea/__init__.py
+++ ichnaea/__init__.py
@@ -1,41 +1,7 @@
 import logging
 
-from pyramid.config import Configurator
-from pyramid.events import NewRequest
-import statsd
-
-from ichnaea.db import CellDB, MeasureDB
-from ichnaea.renderer import DecimalJSON
+from ichnaea.app import main  # NOQA
 
 logger = logging.getLogger('ichnaea')
 
-
-def attach_dbs(event):
-    request = event.request
-    event.request.celldb = request.registry.celldb
-    event.request.measuredb = request.registry.measuredb
-
-
-def main(global_config, **settings):
-    config = Configurator(settings=settings)
-    config.include("cornice")
-    config.scan("ichnaea.views")
-    settings = config.registry.settings
-
-    # statsd settings
-    statsd_settings = {
-        'STATSD_HOST': settings.get('statsd.host', 'localhost'),
-        'STATSD_PORT': int(settings.get('statsd.port', 8125)),
-        'STATSD_SAMPLE_RATE': float(settings.get('statsd.sample', 1.0)),
-        'STATSD_BUCKET_PREFIX': settings.get('statsd.prefix', ''),
-    }
-
-    statsd.init_statsd(statsd_settings)
-
-    config.registry.celldb = CellDB(settings['celldb'])
-    config.registry.measuredb = MeasureDB(settings['measuredb'])
-    config.add_subscriber(attach_dbs, NewRequest)
-
-    # replace json renderer with decimal json variant
-    config.add_renderer('json', DecimalJSON())
-    return config.make_wsgi_app()
+__all__ = ('logger', 'main')
