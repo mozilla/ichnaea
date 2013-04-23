5cef5a2e39d29ec4abb71958b01f1601b0a4c670
Tarek Ziade <tarek@ziade.org>
2013-04-23 17:45:44 +0200

    mocking the actual redis call

--- ichnaea/tests/test_worker.py
+++ ichnaea/tests/test_worker.py
@@ -14,10 +14,24 @@ class TestWorker(unittest.TestCase):
         global_config = {}
         app = main(global_config, celldb='sqlite://', measuredb='sqlite://',
                    async=True, **{'redis.host': '127.0.0.1',
-                                  'redis.port': '6379'})
+                                  'redis.port': '-1'})
         app = TestApp(app)
 
         cell_data = [{"mcc": 123, "mnc": 1, "lac": 2, "cid": 1234}]
-        res = app.post_json('/v1/location/12.345678/23.456789',
-                            {"cell": cell_data}, status=204)
+
+        from redis.client import BasePipeline
+
+        old = BasePipeline.execute
+
+        def _execute(*args, **kw):
+            pass
+
+        BasePipeline.execute = _execute
+
+        try:
+            res = app.post_json('/v1/location/12.345678/23.456789',
+                                {"cell": cell_data}, status=204)
+        finally:
+            BasePipeline.execute = old
+
         self.assertEqual(res.body, '')
