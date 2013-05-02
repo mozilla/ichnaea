b26abffcaf58bc97e3bd64f75040113ed03d3fed
Hanno Schlichting <hanno@hannosch.eu>
2013-05-02 14:45:27 +0200

    avoid some test setup duplication in test_worker

--- ichnaea/tests/test_worker.py
+++ ichnaea/tests/test_worker.py
@@ -1,5 +1,7 @@
 import unittest
 import time
+
+from redis.client import BasePipeline
 from webtest import TestApp
 
 from ichnaea import main
@@ -8,17 +10,20 @@ from ichnaea.worker import _get_db
 
 class TestWorker(unittest.TestCase):
 
+    def _make_one(self, **kw):
+        kw['redis.host'] = '127.0.0.1'
+        kw['redis.port'] = -1
+        app = main({}, celldb='sqlite://', measuredb='sqlite://',
+                   async=True, **kw)
+        return TestApp(app)
+
+    def test_get_db(self):
+        db = _get_db('sqlite:///:memory:')
+        self.assertTrue(db is _get_db('sqlite:///:memory:'))
+
     def test_async_measure(self):
-        global_config = {}
-        app = main(global_config, celldb='sqlite://', measuredb='sqlite://',
-                   async=True, batch_size=10, **{'redis.host': '127.0.0.1',
-                                                 'redis.port': '-1'})
-        app = TestApp(app)
-
+        app = self._make_one(batch_size=10)
         cell_data = [{"mcc": 123, "mnc": 1, "lac": 2, "cid": 1234}]
-
-        from redis.client import BasePipeline
-
         old = BasePipeline.execute
         called = [0]
 
@@ -29,10 +34,11 @@ class TestWorker(unittest.TestCase):
 
         try:
             for i in range(21):
-                res = app.post_json('/v1/submit', {"items": [{"lat": 12.345678,
-                                                   "lon": 23.456789,
-                                                   "cell": cell_data}]},
-                                    status=204)
+                res = app.post_json(
+                    '/v1/submit', {"items": [{"lat": 12.345678,
+                                              "lon": 23.456789,
+                                              "cell": cell_data}]},
+                    status=204)
         finally:
             BasePipeline.execute = old
 
@@ -42,17 +48,8 @@ class TestWorker(unittest.TestCase):
         self.assertEqual(called[0], 2)
 
     def test_async_old_batch(self):
-        global_config = {}
-        app = main(global_config, celldb='sqlite://', measuredb='sqlite://',
-                   async=True, batch_age=.5,
-                   batch_size=10, **{'redis.host': '127.0.0.1',
-                                     'redis.port': '-1'})
-        app = TestApp(app)
-
+        app = self._make_one(batch_size=10, batch_age=0.5)
         cell_data = [{"mcc": 123, "mnc": 1, "lac": 2, "cid": 1234}]
-
-        from redis.client import BasePipeline
-
         old = BasePipeline.execute
         called = [0]
 
@@ -63,10 +60,11 @@ class TestWorker(unittest.TestCase):
 
         try:
             for i in range(4):
-                res = app.post_json('/v1/submit', {"items": [{"lat": 12.345678,
-                                                   "lon": 23.456789,
-                                                   "cell": cell_data}]},
-                                    status=204)
+                res = app.post_json(
+                    '/v1/submit', {"items": [{"lat": 12.345678,
+                                              "lon": 23.456789,
+                                              "cell": cell_data}]},
+                    status=204)
                 time.sleep(.3)
         finally:
             BasePipeline.execute = old
@@ -75,7 +73,3 @@ class TestWorker(unittest.TestCase):
 
         # 1 batches of 2
         self.assertEqual(called[0], 1)
-
-    def test_get_db(self):
-        db = _get_db('sqlite:///:memory:')
-        self.assertTrue(db is _get_db('sqlite:///:memory:'))
