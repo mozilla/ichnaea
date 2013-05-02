7cbf1b91b6649594d371053847188d36e7b1bdbd
Tarek Ziade <tarek@ziade.org>
2013-05-02 19:25:21 +0200

    make sure ichnaea.__init__ is covered

---
+++ ichnaea/tests/test_app.py
@@ -0,0 +1,9 @@
+from unittest2 import TestCase
+from webtest import TestApp
+from ichnaea import application
+
+
+class TestIchnaea(TestCase):
+    def test_ok(self):
+        app = TestApp(application)
+        app.get('/', status=404)
