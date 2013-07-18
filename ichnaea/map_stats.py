998d7a9ccd03e71cbffbcb9d695cba9844326f45
Hanno Schlichting <hanno@hannosch.eu>
2013-07-18 21:51:27 +0200

    only report unique coarse-grained lat/lons

--- ichnaea/map_stats.py
+++ ichnaea/map_stats.py
@@ -7,9 +7,12 @@ from ichnaea.db import Measure
 def map_stats_request(request):
     session = request.database.session()
     query = session.query(Measure.lat, Measure.lon)
+    unique = set()
+    for lat, lon in query:
+        unique.add(((lat // 10000) / 1000.0, (lon // 10000) / 1000.0))
     rows = StringIO()
     csvwriter = csv.writer(rows)
     csvwriter.writerow(('lat', 'lon'))
-    for lat, lon in query:
-        csvwriter.writerow(((lat // 10000) / 1000.0, (lon // 10000) / 1000.0))
+    for lat, lon in unique:
+        csvwriter.writerow((lat, lon))
     return rows.getvalue()
