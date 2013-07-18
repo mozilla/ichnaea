8fc35cbc732ee3f9c21e80d7290ccd905b2817cb
Hanno Schlichting <hanno@hannosch.eu>
2013-07-18 21:45:55 +0200

    work around different sql math in sqlite vs. mysql

--- ichnaea/map_stats.py
+++ ichnaea/map_stats.py
@@ -6,10 +6,10 @@ from ichnaea.db import Measure
 
 def map_stats_request(request):
     session = request.database.session()
-    query = session.query(Measure.lat / 10000, Measure.lon / 10000)
+    query = session.query(Measure.lat, Measure.lon)
     rows = StringIO()
     csvwriter = csv.writer(rows)
     csvwriter.writerow(('lat', 'lon'))
     for lat, lon in query:
-        csvwriter.writerow((lat / 1000.0, lon / 1000.0))
+        csvwriter.writerow(((lat // 10000) / 1000.0, (lon // 10000) / 1000.0))
     return rows.getvalue()
