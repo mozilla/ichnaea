5da228f5ab8e1f0151d5f3dbf877feb07d976158
Hanno Schlichting <hanno@hannosch.eu>
2013-07-19 15:19:43 +0200

    move the map_stats math into the SQL db

--- ichnaea/map_stats.py
+++ ichnaea/map_stats.py
@@ -1,18 +1,17 @@
 import csv
 from cStringIO import StringIO
 
-from ichnaea.db import Measure
+from sqlalchemy.sql.expression import text
 
 
 def map_stats_request(request):
     session = request.database.session()
-    query = session.query(Measure.lat, Measure.lon)
-    unique = set()
-    for lat, lon in query:
-        unique.add(((lat // 100000) / 100.0, (lon // 100000) / 100.0))
+    select = text("select distinct round(lat / 100000) as lat, "
+                  "round(lon / 100000) as lon from measure order by lat, lon")
+    result = session.execute(select)
     rows = StringIO()
     csvwriter = csv.writer(rows)
     csvwriter.writerow(('lat', 'lon'))
-    for lat, lon in unique:
-        csvwriter.writerow((lat, lon))
+    for lat, lon in result.fetchall():
+        csvwriter.writerow((lat / 100.0, lon / 100.0))
     return rows.getvalue()
