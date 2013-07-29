c8a58373d0e4ca1dcaf93bfb39d732643f00b7b9
Hanno Schlichting <hanno@hannosch.eu>
2013-07-29 13:33:22 +0200

    add unit tests for content views

--- ichnaea/content/tests/test_views.py
+++ ichnaea/content/tests/test_views.py
@@ -1,3 +1,6 @@
+from pyramid.testing import DummyRequest
+from pyramid.testing import setUp
+from pyramid.testing import tearDown
 from unittest2 import TestCase
 from webtest import TestApp
 
@@ -10,6 +13,42 @@ def _make_app():
 
 
 class TestContentViews(TestCase):
+
+    def setUp(self):
+        request = DummyRequest()
+        self.config = setUp(request=request)
+
+    def tearDown(self):
+        tearDown()
+
+    def _make_view(self, request):
+        from ichnaea.content.views import ContentViews
+        return ContentViews(request)
+
+    def test_homepage(self):
+        request = DummyRequest()
+        inst = self._make_view(request)
+        result = inst.homepage_view()
+        self.assertEqual(result['page_title'], 'Overview')
+
+    def test_map(self):
+        request = DummyRequest()
+        inst = self._make_view(request)
+        result = inst.map_view()
+        self.assertEqual(result['page_title'], 'Coverage Map')
+
+    def test_stats(self):
+        app = _make_app()
+        request = DummyRequest()
+        request.database = app.app.registry.database
+        inst = self._make_view(request)
+        result = inst.stats_view()
+        self.assertEqual(result['page_title'], 'Statistics')
+        self.assertEqual(result['total_measures'], 0)
+        self.assertEqual(result['leaders'], [])
+
+
+class TestFunctionalContentViews(TestCase):
 
     def test_homepage(self):
         app = _make_app()
