06aed778a810fdbbf70a09067e141b43a1aed718
Hanno Schlichting <hanno@hannosch.eu>
2013-07-29 18:50:46 +0200

    refactor submit/worker code

--- ichnaea/submit.py
+++ ichnaea/submit.py
@@ -2,10 +2,14 @@ import datetime
 import logging
 
 from colander import iso8601
+from colander.iso8601 import parse_date
 
 from ichnaea.db import User
+from ichnaea.db import Measure
+from ichnaea.db import RADIO_TYPE
 from ichnaea.decimaljson import dumps
-from ichnaea.worker import insert_measures
+from ichnaea.decimaljson import loads
+from ichnaea.decimaljson import to_precise_int
 
 logger = logging.getLogger('ichnaea')
 
@@ -54,3 +58,44 @@ def submit_request(request):
 
     insert_measures(measures, session)
     session.commit()
+
+
+def process_wifi(values):
+    # convert frequency into channel numbers
+    result = []
+    for entry in values:
+        # always remove frequency
+        freq = entry.pop('frequency')
+        # if no explicit channel was given, calculate
+        if freq and not entry['channel']:
+            if 2411 < freq < 2473:
+                # 2.4 GHz band
+                entry['channel'] = (freq - 2407) // 5
+            elif 5169 < freq < 5826:
+                # 5 GHz band
+                entry['channel'] = (freq - 5000) // 5
+        result.append(entry)
+    return result
+
+
+def insert_measures(measures, session):
+    for data in measures:
+        if isinstance(data, basestring):
+            data = loads(data)
+        measure = Measure()
+        time = data['time']
+        if isinstance(time, basestring):
+            time = parse_date(time)
+        measure.time = time
+        measure.token = data['token']
+        measure.lat = to_precise_int(data['lat'])
+        measure.lon = to_precise_int(data['lon'])
+        measure.accuracy = data['accuracy']
+        measure.altitude = data['altitude']
+        measure.altitude_accuracy = data['altitude_accuracy']
+        if data.get('cell'):
+            measure.radio = RADIO_TYPE.get(data['radio'], 0)
+            measure.cell = dumps(data['cell'])
+        if data.get('wifi'):
+            measure.wifi = dumps(process_wifi(data['wifi']))
+        session.add(measure)
