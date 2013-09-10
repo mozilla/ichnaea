875b4ed4d8e427f4844c9a4cbc02813b09475087
Hanno Schlichting <hanno@hannosch.eu>
2013-09-10 11:54:01 -0700

    update importer to use bssid and unrelated cleanup

--- ichnaea/importer.py
+++ ichnaea/importer.py
@@ -6,15 +6,11 @@ import sys
 from colander import iso8601
 from konfig import Config
 
-from ichnaea.db import Database
+from ichnaea.db import (
+    Database,
+    normalize_wifi_key,
+)
 from ichnaea.submit import process_measure
-
-
-def _int(value):
-    try:
-        return int(value)
-    except ValueError:
-        return 0
 
 
 def load_file(session, source_file, batch_size=10000):
@@ -29,27 +25,21 @@ def load_file(session, source_file, batch_size=10000):
         for fields in reader:
             try:
                 time = int(fields[0])
-                if time == 0:
+                if time == 0:  # pragma: no cover
                     # unknown time gets an old date
                     time = utcmin
                 else:
                     # convert from unixtime to utc
                     time = datetime.datetime.utcfromtimestamp(time)
-                key = str(fields[1])
-                if len(key) != 40:  # pragma: no cover
-                    print "too short key: %s" % key
+
+                key = normalize_wifi_key(str(fields[6]))
+                if key == '000000000000':  # pragma: no cover
                     continue
+
                 lat = fields[2]
                 lon = fields[3]
                 signal = int(fields[4])
-                if signal > 0 or signal < -140:
-                    signal = -140
                 channel = int(fields[5])
-                if channel < 0:
-                    channel = 0
-                ssid = str(fields[6])
-                if ssid == '00:00:00:00:00:00':
-                    continue
                 wifi = dict(
                     key=key,
                     channel=channel,
