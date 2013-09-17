fafc15f0b9f80464d845ee58190f364ae4d3491f
Hanno Schlichting <hanno@hannosch.eu>
2013-09-17 10:58:21 -0700

    refactor `insert_wifi_measure` into smaller pieces

--- ichnaea/service/submit/tasks.py
+++ ichnaea/service/submit/tasks.py
@@ -10,15 +10,58 @@ from ichnaea.tasks import DatabaseTask
 from ichnaea.worker import celery
 
 
+def convert_frequency(entry):
+    freq = entry.pop('frequency', 0)
+    # if no explicit channel was given, calculate
+    if freq and not entry['channel']:
+        if 2411 < freq < 2473:
+            # 2.4 GHz band
+            entry['channel'] = (freq - 2407) // 5
+        elif 5169 < freq < 5826:
+            # 5 GHz band
+            entry['channel'] = (freq - 5000) // 5
+
+
+def update_wifi_measure_count(wifi_key, wifis, session):
+    # side-effect, modifies wifis
+    if wifi_key in wifis:
+        wifi = wifis[wifi_key]
+        wifi.new_measures = Wifi.new_measures + 1
+        wifi.total_measures = Wifi.total_measures + 1
+    else:
+        wifis[wifi_key] = wifi = Wifi(
+            key=wifi_key, new_measures=1, total_measures=1)
+        session.add(wifi)
+
+
+def create_wifi_measure(measure_data, entry):
+    return WifiMeasure(
+        measure_id=measure_data['id'],
+        created=decode_datetime(measure_data.get('created', '')),
+        lat=measure_data['lat'],
+        lon=measure_data['lon'],
+        time=decode_datetime(measure_data.get('time', '')),
+        accuracy=measure_data.get('accuracy', 0),
+        altitude=measure_data.get('altitude', 0),
+        altitude_accuracy=measure_data.get('altitude_accuracy', 0),
+        id=entry.get('id', None),
+        key=entry['key'],
+        channel=entry.get('channel', 0),
+        signal=entry.get('signal', 0),
+    )
+
+
 @celery.task(base=DatabaseTask, ignore_result=True)
 def insert_wifi_measure(measure_data, entries):
     wifi_measures = []
     wifi_keys = set([e['key'] for e in entries])
     try:
         with insert_wifi_measure.db_session() as session:
+            # did we get measures for blacklisted wifis?
             blacked = session.query(WifiBlacklist.key).filter(
                 WifiBlacklist.key.in_(wifi_keys)).all()
             blacked = set([b[0] for b in blacked])
+            # do we already know about these wifis?
             wifis = session.query(Wifi.key, Wifi).filter(
                 Wifi.key.in_(wifi_keys))
             wifis = dict(wifis.all())
@@ -28,39 +71,10 @@ def insert_wifi_measure(measure_data, entries):
                 if wifi_key in blacked:
                     continue
                 # convert frequency into channel numbers and remove frequency
-                freq = entry.pop('frequency', 0)
-                # if no explicit channel was given, calculate
-                if freq and not entry['channel']:
-                    if 2411 < freq < 2473:
-                        # 2.4 GHz band
-                        entry['channel'] = (freq - 2407) // 5
-                    elif 5169 < freq < 5826:
-                        # 5 GHz band
-                        entry['channel'] = (freq - 5000) // 5
-                wifi_measure = WifiMeasure(
-                    measure_id=measure_data['id'],
-                    created=decode_datetime(measure_data.get('created', '')),
-                    lat=measure_data['lat'],
-                    lon=measure_data['lon'],
-                    time=decode_datetime(measure_data.get('time', '')),
-                    accuracy=measure_data.get('accuracy', 0),
-                    altitude=measure_data.get('altitude', 0),
-                    altitude_accuracy=measure_data.get('altitude_accuracy', 0),
-                    id=entry.get('id', None),
-                    key=wifi_key,
-                    channel=entry.get('channel', 0),
-                    signal=entry.get('signal', 0),
-                )
-                wifi_measures.append(wifi_measure)
+                convert_frequency(entry)
+                wifi_measures.append(create_wifi_measure(measure_data, entry))
                 # update new/total measure counts
-                if wifi_key in wifis:
-                    wifi = wifis[wifi_key]
-                    wifi.new_measures = Wifi.new_measures + 1
-                    wifi.total_measures = Wifi.total_measures + 1
-                else:
-                    wifis[wifi_key] = wifi = Wifi(
-                        key=wifi_key, new_measures=1, total_measures=1)
-                    session.add(wifi)
+                update_wifi_measure_count(wifi_key, wifis, session)
 
             session.add_all(wifi_measures)
             session.commit()
