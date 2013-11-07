20cb141ed6ba68ff118895576e3cd2276c875cdb
Hanno Schlichting <hanno@hannosch.eu>
2013-11-07 17:10:23 +0100

    use `on_duplicate` trick to update scores
    removes the insert vs. update vs. concurrency code and assorted bugs

--- ichnaea/service/submit/utils.py
+++ ichnaea/service/submit/utils.py
@@ -8,20 +8,8 @@ from ichnaea.content.models import (
 
 def process_score(userid, points, session, key='location'):
     utcday = datetime.datetime.utcnow().date()
-    key = SCORE_TYPE[key]
-    rows = session.query(Score).filter(
-        Score.userid == userid).filter(
-        Score.key == key).filter(
-        Score.time == utcday).limit(1).with_lockmode('update')
-    old = rows.first()
-    if old:
-        # update score
-        if isinstance(old.value, (int, long)):
-            old.value = Score.value + points
-        else:
-            # already a sql expression
-            old.value += points
-    else:
-        score = Score(userid=userid, key=key, time=utcday, value=points)
-        session.add(score)
+    stmt = Score.__table__.insert(
+        on_duplicate='value = value + %s' % int(points)).values(
+        userid=userid, key=SCORE_TYPE[key], time=utcday, value=points)
+    session.execute(stmt)
     return points
