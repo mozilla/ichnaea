8cd648d745db16908a7af3d88f059556bdcfb7ae
Victor Ng <victor@crankycoder.com>
2014-01-20 15:31:56 -0500

    merged from master

--- ichnaea/worker.py
+++ ichnaea/worker.py
@@ -14,6 +14,7 @@ from ichnaea.heka_logging import configure_heka
 CELERY_IMPORTS = [
     'ichnaea.tasks',
     'ichnaea.content.tasks',
+    'ichnaea.backfill.tasks',
     'ichnaea.service.submit.tasks',
 ]
 
@@ -68,6 +69,10 @@ CELERYBEAT_SCHEDULE = {
         'schedule': timedelta(seconds=329),  # 7*47
         'args': (2, 10, 5000),
     },
+    'backfill-celltower-info': {
+        'task': 'ichnaea.backfill.tasks.do_backfill',
+        'schedule': crontab(hour=0, minute=15),
+    }
 }
 
 celery = Celery('ichnaea.worker')
