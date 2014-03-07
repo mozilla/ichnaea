81ddf71c528c5e155195c4bf7d1aa23cecc8002f
Hanno Schlichting <hanno@hannosch.eu>
2014-03-07 11:29:42 +0100

    Avoid docstrings for tests, or the testrunner won't report the test name.

--- ichnaea/tests/test_geocalc.py
+++ ichnaea/tests/test_geocalc.py
@@ -6,9 +6,8 @@ from ichnaea.geocalc import distance
 class TestDistance(unittest.TestCase):
 
     def test_simple_distance(self):
-        """
-        This is a simple case where the points are close to each other.
-        """
+        # This is a simple case where the points are close to each other.
+
         lat1 = 44.0337065
         lon1 = -79.4908184
         lat2 = 44.0347065
@@ -18,12 +17,11 @@ class TestDistance(unittest.TestCase):
         self.assertEqual(sdelta, '0.1369')
 
     def test_antipodal(self):
-        """
-        Antipodal points (opposite sides of the planet) have a round off
-        error with the standard haversine calculation which is extremely
-        old and assumes we are using fixed precision math instead of IEEE
-        floats.
-        """
+        # Antipodal points (opposite sides of the planet) have a round off
+        # error with the standard haversine calculation which is extremely
+        # old and assumes we are using fixed precision math instead of IEEE
+        # floats.
+
         lat1 = 90.0
         lon1 = 0.0
         lat2 = -90.0
@@ -33,11 +31,10 @@ class TestDistance(unittest.TestCase):
         self.assertEqual(sdelta, '20015.0868')
 
     def test_out_of_range(self):
-        """
-        We don't always sanitize the incoming data and thus have to deal
-        with some invalid coordinates. Make sure the distance function
-        doesn't error out on us.
-        """
+        # We don't always sanitize the incoming data and thus have to deal
+        # with some invalid coordinates. Make sure the distance function
+        # doesn't error out on us.
+
         lat1 = -100.0
         lon1 = -186.0
         lat2 = 0.0
