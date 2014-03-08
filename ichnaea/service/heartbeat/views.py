7fccb89a6d70a3d95071e80e73c34d954a36d01e
Graydon Hoare <graydon@mozilla.com>
2014-03-07 16:32:49 -0800

    Change heartbeat view to do a DB query, close #128

--- ichnaea/service/heartbeat/views.py
+++ ichnaea/service/heartbeat/views.py
@@ -1,6 +1,10 @@
 import socket
 
 from pyramid.view import view_config
+from pyramid.response import Response
+from pyramid.httpexceptions import HTTPServiceUnavailable
+from sqlalchemy.sql import select
+from sqlalchemy.sql import func
 
 LOCAL_FQDN = socket.getfqdn()
 
@@ -11,4 +15,14 @@ def configure_heartbeat(config):
 
 @view_config(renderer='json', name="__heartbeat__")
 def heartbeat_view(request):
-    return {'status': 'OK', 'hostname': LOCAL_FQDN}
+
+    session = request.db_slave_session
+    conn = session.connection()
+
+    try:
+        if conn.execute(select([func.now()])).first() is None:
+            return HTTPServiceUnavailable()
+        else:
+            return {'status': 'OK', 'hostname': LOCAL_FQDN}
+    except:
+        return HTTPServiceUnavailable()
