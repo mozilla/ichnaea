010e1958d0e4936a87e9950c41fe4f14d538232e
Victor Ng <victor@crankycoder.com>
2014-04-22 14:02:17 -0400

    init checkin of alembic scripts to add api_key table

---
+++ alembic/versions/2a311d11a90d_create_valid_api_key_table.py
@@ -0,0 +1,24 @@
+"""create valid api key table
+
+Revision ID: 2a311d11a90d
+Revises: None
+Create Date: 2014-04-22 13:50:03.556305
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2a311d11a90d'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table('api_key',
+                    sa.Column('valid_key', sa.String(36), primary_key=True),
+                    )
+
+
+def downgrade():
+    op.drop_table('api_key')
