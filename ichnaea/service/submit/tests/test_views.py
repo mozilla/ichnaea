e118beb1a3a759e5c6d3442d4b82f9a61b2ff606
Graydon Hoare <graydon@mozilla.com>
2014-05-06 16:58:30 -0700

    Add testcase for normalization of cell measures.

--- ichnaea/service/submit/tests/test_views.py
+++ ichnaea/service/submit/tests/test_views.py
@@ -16,6 +16,7 @@ from ichnaea.models import (
     RADIO_TYPE,
     WifiMeasure,
     ApiKey,
+    from_degrees,
 )
 from ichnaea.decimaljson import (
     dumps,
@@ -596,3 +597,187 @@ class TestSubmit(CeleryAppTestCase):
         self.assertEqual(len(cell_result), 0)
         wifi_result = session.query(WifiMeasure).all()
         self.assertEqual(len(wifi_result), 1)
+
+    def check_normalized_cell(self, measure, cell, expect, status=204):
+        measure = measure.copy()
+        measure.update({'cell': [cell]})
+        session = self.db_master_session
+        self.app.post_json('/v1/submit', {"items": [measure]},
+                           status=status)
+        cell_result = session.query(CellMeasure).all()
+        if expect is None:
+            self.assertEqual(len(cell_result), 0)
+        else:
+            self.assertEqual(len(cell_result), 1)
+            item = cell_result[0]
+            for (k, v) in expect.items():
+                self.assertEqual(getattr(item, k), v)
+            session.query(CellMeasure).delete()
+
+    def test_normalize_cells(self):
+
+        radio_pairs = [('gsm', 0),
+                       ('cdma', 1),
+                       ('umts', 2),
+                       ('wcdma', 2),
+                       ('lte', 3),
+                       ('wimax', None),
+                       ('', -1),
+                       ('hspa', None),
+                       ('n/a', None)]
+
+        valid_mccs = [1, 25, 999]
+        invalid_mccs = [-10, 0, 1000, 3456]
+
+        valid_mncs = [0, 542, 32767]
+        invalid_mncs = [-10, -1, 32768, 93870]
+
+        valid_lacs = [0, 763, 65535]
+        invalid_lacs = [-1, -10, 65536, 987347]
+
+        valid_cids = [0, 12345, 268435455]
+        invalid_cids = [-10, -1, 268435456, 498169872]
+
+        valid_pscs = [0, 120, 512]
+        invalid_pscs = [-1, 513, 4456]
+
+        valid_latitudes = [-90.0, -45.0, 0.0, 45.0, 90.0]
+        invalid_latitudes = [-100.0, -90.1, 90.1, 100.0]
+
+        valid_longitudes = [-180.0, -90.0, 0.0, 90.0, 180.0]
+        invalid_longitudes = [-190.0, -180.1, 180.1, 190]
+
+        valid_accuracies = [0, 1, 100, 10000]
+        invalid_accuracies = [-10, -1, 5000000]
+
+        valid_altitudes = [-100, -1, 0, 10, 100]
+        invalid_altitudes = [-20000, 200000]
+
+        valid_altitude_accuracies = [0, 1, 100, 1000]
+        invalid_altitude_accuracies = [-10, -1, 500000]
+
+        valid_asus = [0, 10, 31]
+        invalid_asus = [-10, -1, 32, 100]
+
+        valid_tas = [0, 15, 63]
+        invalid_tas = [-10, -1, 64, 100]
+
+        valid_signals = [-200, -100, -1]
+        invalid_signals = [-300, -201, 0, 10]
+
+        def make_submission(**kw):
+            time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
+            measure = dict(radio='umts',
+                           lat=49.25, lon=123.10, accuracy=120,
+                           altitude=220, altitude_accuracy=10,
+                           time=time)
+            cell = dict(mcc=302, mnc=220, lac=12345, cid=34567, psc=-1,
+                        asu=15, signal=-83, ta=5)
+            for (k, v) in kw.items():
+                if k in measure:
+                    measure[k] = v
+                else:
+                    cell[k] = v
+            return (measure, cell)
+
+        # Try all radio values
+        for (radio, v) in radio_pairs:
+            (measure, cell) = make_submission(radio=radio)
+            if v is None:
+                self.check_normalized_cell(measure, cell, None, status=400)
+            else:
+                self.check_normalized_cell(measure, cell, dict(radio=v))
+
+        # Try all valid (mcc, mnc) pairs
+        for mcc in valid_mccs:
+            for mnc in valid_mncs:
+                (measure, cell) = make_submission(mcc=mcc, mnc=mnc)
+                self.check_normalized_cell(measure, cell, dict(mcc=mcc,
+                                                               mnc=mnc))
+
+        # Try all invalid mcc variants individually
+        for mcc in invalid_mccs:
+            (measure, cell) = make_submission(mcc=mcc)
+            self.check_normalized_cell(measure, cell, None)
+
+        # Try all invalid mnc variants individually
+        for mnc in invalid_mncs:
+            (measure, cell) = make_submission(mnc=mnc)
+            self.check_normalized_cell(measure, cell, None)
+
+        # Try all valid (lac, cid) pairs, with invalid pscs
+        for lac in valid_lacs:
+            for cid in valid_cids:
+                for psc in invalid_pscs:
+                    (measure, cell) = make_submission(lac=lac, cid=cid,
+                                                      psc=psc)
+                    self.check_normalized_cell(measure, cell, dict(lac=lac,
+                                                                   cid=cid,
+                                                                   psc=-1))
+
+        # Try all invalid lacs, with an invalid psc
+        for lac in invalid_lacs:
+            for psc in invalid_pscs:
+                (measure, cell) = make_submission(lac=lac, psc=psc)
+                self.check_normalized_cell(measure, cell, None)
+
+        # Try all invalid cids, with an invalid psc
+        for cid in invalid_cids:
+            for psc in invalid_pscs:
+                (measure, cell) = make_submission(cid=cid, psc=psc)
+                self.check_normalized_cell(measure, cell, None)
+
+        # Try all invalid lacs, with a valid psc
+        for lac in invalid_lacs:
+            for psc in valid_pscs:
+                (measure, cell) = make_submission(lac=lac, psc=psc)
+                self.check_normalized_cell(measure, cell, dict(lac=-1,
+                                                               psc=psc))
+
+        # Try all invalid cids, with a valid psc
+        for cid in invalid_cids:
+            for psc in valid_pscs:
+                (measure, cell) = make_submission(cid=cid, psc=psc)
+                self.check_normalized_cell(measure, cell, dict(cid=-1,
+                                                               psc=psc))
+
+        # Try all valid (lat, lon) pairs
+        for lat in valid_latitudes:
+            for lon in valid_longitudes:
+                (measure, cell) = make_submission(lat=lat, lon=lon)
+                self.check_normalized_cell(measure, cell,
+                                           dict(lat=from_degrees(lat),
+                                                lon=from_degrees(lon)))
+
+        # Try all invalid latitudes individually
+        for lat in invalid_latitudes:
+            (measure, cell) = make_submission(lat=lat)
+            self.check_normalized_cell(measure, cell, None)
+
+        # Try all invalid longitudes individually
+        for lon in invalid_longitudes:
+            (measure, cell) = make_submission(lon=lon)
+            self.check_normalized_cell(measure, cell, None)
+
+        # Try all 'nice to have' valid fields individually
+        for (k, vs) in [('accuracy', valid_accuracies),
+                        ('altitude', valid_altitudes),
+                        ('altitude_accuracy', valid_altitude_accuracies),
+                        ('asu', valid_asus),
+                        ('ta', valid_tas),
+                        ('signal', valid_signals)]:
+            for v in vs:
+                (measure, cell) = make_submission(**{k: v})
+                self.check_normalized_cell(measure, cell, {k: v})
+
+        # Try all 'nice to have' invalid fields individually
+        for (k, vs, x) in [('accuracy', invalid_accuracies, 0),
+                           ('altitude', invalid_altitudes, 0),
+                           ('altitude_accuracy',
+                            invalid_altitude_accuracies, 0),
+                           ('asu', invalid_asus, -1),
+                           ('ta', invalid_tas, 0),
+                           ('signal', invalid_signals, 0)]:
+            for v in vs:
+                (measure, cell) = make_submission(**{k: v})
+                self.check_normalized_cell(measure, cell, {k: x})
