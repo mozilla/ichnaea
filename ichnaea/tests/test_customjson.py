3f257fe26720e630a13f84d110adbf413573500c
Graydon Hoare <graydon@mozilla.com>
2014-06-09 12:00:35 -0700

    Use custom json only when rendering responses with 'accuracy' field.

--- ichnaea/tests/test_customjson.py
+++ ichnaea/tests/test_customjson.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from datetime import date
 from datetime import datetime
 from datetime import timedelta
+import sys
 
 from ichnaea.customjson import (
     dumps,
@@ -31,12 +32,25 @@ class TestRenderer(TestCase):
     def test_high_precision(self):
         render = self._make_one()
         d = 12.345678
-        self.assertEqual(render({'a': d}, {}), '{"a": 12.345678}')
+        self.assertEqual(render({'accuracy': d}, {}),
+                         '{"accuracy": 12.345678}')
+
+    def test_no_special_treatment_without_accuracy_field(self):
+        # This tests that, when we're rendering a dict to json with no
+        # 'accuracy' field, we do not apply any special processing to
+        # floats (namely, we get ugly float representation on python2.6)
+        render = self._make_one()
+        d = 12.345678
+        if sys.version_info < (2, 7):
+            expect = '{"z": 12.345677999999999}'
+        else:
+            expect = '{"z": 12.345678}'
+        self.assertEqual(render({'z': d}, {}), expect)
 
     def test_low_precision(self):
         render = self._make_one()
         d = 12.34
-        self.assertEqual(render({'a': d}, {}), '{"a": 12.34}')
+        self.assertEqual(render({'accuracy': d}, {}), '{"accuracy": 12.34}')
 
     def test_error(self):
         self.assertRaises(TypeError, dumps, timedelta(days=1))
