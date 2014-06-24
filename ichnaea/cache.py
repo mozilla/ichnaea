a529f6ed248b50950e2eaddd3edcfa219df9a9f6
Hanno Schlichting <hanno@hannosch.eu>
2014-06-24 08:13:59 +0200

    Tweak redis connection pool settings to use timeouts and keep-alive.

--- ichnaea/cache.py
+++ ichnaea/cache.py
@@ -7,7 +7,12 @@ def redis_con(redis_url, registry):
     r_host = r_url.netloc.split(":")[0]
     r_port = int(r_url.netloc.split(":")[1])
     r_db = int(r_url.path[1:])
-    pool = redis.ConnectionPool(max_connections=100)
+    pool = redis.ConnectionPool(
+        max_connections=100,
+        socket_timeout=10.0,
+        socket_connect_timeout=30.0,
+        socket_keepalive=True,
+    )
     registry.redis_pool = pool
     return redis.StrictRedis(host=r_host,
                              port=r_port,
