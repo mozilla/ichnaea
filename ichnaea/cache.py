bfe65c4ed6cd348affb11df13f4de2c07f37a2dc
Hanno Schlichting <hanno@hannosch.eu>
2014-06-24 09:55:40 +0200

    Fall back to default port and db in redis connection setup.

--- ichnaea/cache.py
+++ ichnaea/cache.py
@@ -3,15 +3,22 @@ import urlparse
 
 
 def redis_client(redis_url):
-    r_url = urlparse.urlparse(redis_url)
-    r_host = r_url.netloc.split(":")[0]
-    r_port = int(r_url.netloc.split(":")[1])
-    r_db = int(r_url.path[1:])
+    url = urlparse.urlparse(redis_url)
+    netloc = url.netloc.split(":")
+    host = netloc[0]
+    if len(netloc) > 1:
+        port = int(netloc[1])
+    else:
+        port = 6379
+    if len(url.path) > 1:
+        db = int(url.path[1:])
+    else:
+        db = 0
     pool = redis.ConnectionPool(
         max_connections=100,
-        host=r_host,
-        port=r_port,
-        db=r_db,
+        host=host,
+        port=port,
+        db=db,
         socket_timeout=10.0,
         socket_connect_timeout=30.0,
         socket_keepalive=True,
