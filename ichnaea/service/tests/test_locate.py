12aa560fd0eafa9bcfc7c7ac223b42740bd8c7b8
Hanno Schlichting <hanno@hannosch.eu>
2014-07-28 17:22:31 +0200

    Move some detailed tests over to new test_locate module.

--- ichnaea/service/tests/test_locate.py
+++ ichnaea/service/tests/test_locate.py
@@ -1,3 +1,5 @@
+import random
+
 from ichnaea.geocalc import maximum_country_radius
 from ichnaea.geoip import GeoIPMock
 from ichnaea.models import (
@@ -307,3 +309,180 @@ class TestSearchAllSources(DBTestCase):
                 'geolocate.geoip_country_found',
             ],
         )
+
+    def test_wifi_too_few_candidates(self):
+        session = self.db_slave_session
+        wifis = [
+            Wifi(key="001122334455", lat=1.0, lon=1.0),
+            Wifi(key="112233445566", lat=1.001, lon=1.002),
+        ]
+        session.add_all(wifis)
+        session.flush()
+
+        result = locate.search_all_sources(
+            session, 'geolocate',
+            {'wifi': [{"key": "001122334455"}]})
+
+        self.assertTrue(result is None)
+        self.check_stats(
+            counter=[
+                'geolocate.miss',
+                'geolocate.no_wifi_found',
+            ],
+        )
+
+    def test_wifi_too_few_matches(self):
+        session = self.db_slave_session
+        wifis = [
+            Wifi(key="001122334455", lat=1.0, lon=1.0),
+            Wifi(key="112233445566", lat=1.001, lon=1.002),
+            Wifi(key="223344556677", lat=None, lon=None),
+        ]
+        session.add_all(wifis)
+        session.flush()
+
+        result = locate.search_all_sources(
+            session, 'geolocate',
+            {'wifi': [{"key": "001122334455"}, {"key": "223344556677"}]})
+
+        self.assertTrue(result is None)
+        self.check_stats(
+            counter=[
+                'geolocate.miss',
+                'geolocate.no_wifi_found',
+            ],
+        )
+
+    def test_wifi_ignore_outlier(self):
+        session = self.db_slave_session
+        wifis = [
+            Wifi(key="001122334455", lat=1.0, lon=1.0),
+            Wifi(key="112233445566", lat=1.001, lon=1.002),
+            Wifi(key="223344556677", lat=1.002, lon=1.004),
+            Wifi(key="334455667788", lat=2.0, lon=2.0),
+        ]
+        session.add_all(wifis)
+        session.flush()
+
+        result = locate.search_all_sources(
+            session, 'geolocate',
+            {'wifi': [
+                {"key": "001122334455"}, {"key": "112233445566"},
+                {"key": "223344556677"}, {"key": "334455667788"},
+            ]})
+
+        self.assertEqual(result,
+                         {'lat': 1.001,
+                          'lon': 1.002,
+                          'accuracy': 248.6090897})
+
+    def test_wifi_prefer_cluster_with_better_signals(self):
+        session = self.db_slave_session
+        wifis = [
+            Wifi(key="A1", lat=1.0, lon=1.0),
+            Wifi(key="B2", lat=1.001, lon=1.002),
+            Wifi(key="C3", lat=1.002, lon=1.004),
+            Wifi(key="D4", lat=2.0, lon=2.0),
+            Wifi(key="E5", lat=2.001, lon=2.002),
+            Wifi(key="F6", lat=2.002, lon=2.004),
+        ]
+        session.add_all(wifis)
+        session.flush()
+
+        result = locate.search_all_sources(
+            session, 'geolocate',
+            {'wifi': [
+                {"key": "A1", "signal": -100},
+                {"key": "D4", "signal": -80},
+                {"key": "B2", "signal": -100},
+                {"key": "E5", "signal": -90},
+                {"key": "C3", "signal": -100},
+                {"key": "F6", "signal": -54},
+            ]})
+
+        self.assertEqual(result,
+                         {'lat': 2.001,
+                          'lon': 2.002,
+                          'accuracy': 248.51819})
+
+    def test_wifi_prefer_larger_cluster_over_high_signal(self):
+        session = self.db_slave_session
+        wifis = [Wifi(key="A%d" % i,
+                      lat=1 + i * 0.000010,
+                      lon=1 + i * 0.000012)
+                 for i in range(5)]
+        wifis += [
+            Wifi(key="D4", lat=2.0, lon=2.0),
+            Wifi(key="E5", lat=2.001, lon=2.002),
+            Wifi(key="F6", lat=2.002, lon=2.004),
+        ]
+        session.add_all(wifis)
+        session.flush()
+
+        measures = [dict(key="A%d" % i,
+                         signal=-80)
+                    for i in range(5)]
+        measures += [
+            dict(key="D4", signal=-75),
+            dict(key="E5", signal=-74),
+            dict(key="F6", signal=-73)
+        ]
+        random.shuffle(measures)
+
+        result = locate.search_all_sources(
+            session, 'geolocate',
+            {'wifi': measures})
+
+        self.assertEqual(result,
+                         {'lat': 1.00002,
+                          'lon': 1.000024,
+                          'accuracy': WIFI_MIN_ACCURACY})
+
+    def test_wifi_only_use_top_five_signals_in_noisy_cluster(self):
+        session = self.db_slave_session
+        # all these should wind up in the same cluster since
+        # clustering threshold is 500m and the 10 wifis are
+        # spaced in increments of (+1m, +1.2m)
+        wifis = [Wifi(key="A%d" % i,
+                      lat=1 + i * 0.000010,
+                      lon=1 + i * 0.000012)
+                 for i in range(10)]
+        session.add_all(wifis)
+        session.commit()
+        measures = [dict(key="A%d" % i,
+                         signal=-80)
+                    for i in range(5, 10)]
+        measures += [
+            dict(key="A0", signal=-75),
+            dict(key="A1", signal=-74),
+            dict(key="A2", signal=-73),
+            dict(key="A3", signal=-72),
+            dict(key="A4", signal=-71),
+        ]
+        random.shuffle(measures)
+
+        result = locate.search_all_sources(
+            session, 'geolocate',
+            {'wifi': measures})
+
+        self.assertEqual(result,
+                         {'lat': 1.00002,
+                          'lon': 1.000024,
+                          'accuracy': WIFI_MIN_ACCURACY})
+
+    def test_wifi_not_closeby(self):
+        session = self.db_slave_session
+        wifis = [
+            Wifi(key="A1", lat=1.0, lon=1.0),
+            Wifi(key="B2", lat=1.001, lon=1.002),
+            Wifi(key="C3", lat=2.002, lon=2.004),
+            Wifi(key="D4", lat=2.0, lon=2.0),
+        ]
+        session.add_all(wifis)
+        session.flush()
+
+        result = locate.search_all_sources(
+            session, 'geolocate',
+            {'wifi': [{"key": "A1"}, {"key": "C3"}]})
+
+        self.assertTrue(result is None)
