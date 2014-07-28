e9f3b6f9eb59ef7290498e8ceaf81c2bc66c8f59
Hanno Schlichting <hanno@hannosch.eu>
2014-07-28 22:36:57 +0200

    Update gunicorn timeout after gunicorn issue was answered.

--- ichnaea/gunicorn_config.py
+++ ichnaea/gunicorn_config.py
@@ -6,9 +6,7 @@
 worker_class = "sync"
 
 # Set timeout to the same value as the default one from Amazon ELB (60 secs).
-# It should be 60 seconds, but gunicorn halves the configured value,
-# see https://github.com/benoitc/gunicorn/issues/829
-timeout = 120
+timeout = 60
 
 # Recycle worker processes after 100k requests to prevent memory leaks
 # from effecting us
