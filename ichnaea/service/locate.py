980f28d13be44b74398889016255f9b5f823544b
Graydon Hoare <graydon@mozilla.com>
2014-07-31 14:02:11 -0700

    Move cluster_elements from util.py to to service/locate.py

--- ichnaea/service/locate.py
+++ ichnaea/service/locate.py
@@ -25,7 +25,6 @@ from ichnaea.models import (
     DEGREE_DECIMAL_PLACES,
 )
 from ichnaea.stats import get_stats_client
-from ichnaea.util import cluster_elements
 
 # parameters for wifi clustering
 MAX_WIFI_CLUSTER_KM = 0.5
@@ -248,6 +247,48 @@ def search_cell_lac(session, lacs):
         'lon': lac.lon,
         'accuracy': accuracy,
     }
+
+
+def cluster_elements(elts, dist, thresh):
+    """
+    Generic pairwise clustering routine.
+
+    :param elts: A list of elemenets to cluster.
+    :param dist: A pairwise distance function over elements.
+    :param thresh: A numeric threshold for clustering;
+                   clusters P, Q will be joined if dist(a,b) <= thresh,
+                   for any a in P, b in Q.
+
+    :returns: A list of lists of elements, each sub-list being a cluster.
+    """
+    elts = list(elts)
+    distance_matrix = [[dist(a, b) for a in elts] for b in elts]
+    n = len(elts)
+    clusters = [[i] for i in range(n)]
+
+    def cluster_distance(a, b):
+        return min([distance_matrix[i][j] for i in a for j in b])
+
+    merged_one = True
+    while merged_one:
+        merged_one = False
+        m = len(clusters)
+        for i in range(m):
+            if merged_one:
+                break
+            for j in range(m):
+                if merged_one:
+                    break
+                if i == j:
+                    continue
+                a = clusters[i]
+                b = clusters[j]
+                if cluster_distance(a, b) <= thresh:
+                    clusters.pop(j)
+                    a.extend(b)
+                    merged_one = True
+
+    return [[elts[i] for i in c] for c in clusters]
 
 
 def filter_bssids_by_similarity(bs):
