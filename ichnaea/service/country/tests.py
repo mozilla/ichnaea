bcade1576e90bf576ba41f1cf147954ee688af0f
Hanno Schlichting <hanno@hannosch.eu>
2014-12-19 18:27:52 +0100

    First wave of optimizations to minimize DB lookups and stats for country API.

--- ichnaea/service/country/tests.py
+++ ichnaea/service/country/tests.py
@@ -46,38 +46,41 @@ class TestCountry(AppTestCase, CountryBase):
     def test_geoip(self):
         result = self._make_geoip_query(status=200)
         self._check_geoip_result(result, status=200)
-        self.check_db_calls(master=0, slave=1)
+        self.check_db_calls(master=0, slave=0)
         self.check_stats(
             timer=['request.v1.country'],
-            counter=['country.api_key.test', 'country.geoip_hit'])
+            counter=[('country.api_key.test', 0),
+                     'country.geoip_hit'])
 
     def test_geoip_miss(self):
         result = self._make_geoip_query(ip=None, status=404)
         self._check_geoip_result(result, status=404)
-        self.check_db_calls(master=0, slave=1)
+        self.check_db_calls(master=0, slave=0)
         self.check_stats(
-            counter=['country.api_key.test', 'country.miss'])
+            counter=[('country.api_key.test', 0),
+                     'country.miss'])
 
     def test_incomplete_request_means_geoip(self):
         result = self._make_geoip_query(data={"wifiAccessPoints": []})
         self._check_geoip_result(result, status=200)
-        self.check_db_calls(master=0, slave=1)
-        self.check_stats(
-            counter=['country.api_key.test', 'country.geoip_hit'])
-
-    def test_no_api_key(self):
-        result = self._make_geoip_query(api_key=None, status=400)
-        self._check_geoip_result(result, status=400)
         self.check_db_calls(master=0, slave=0)
         self.check_stats(
-            counter=['country.no_api_key'])
+            counter=[('country.api_key.test', 0),
+                     'country.geoip_hit'])
+
+    def test_no_api_key(self):
+        result = self._make_geoip_query(api_key=None, status=200)
+        self._check_geoip_result(result, status=200)
+        self.check_db_calls(master=0, slave=0)
+        self.check_stats(
+            counter=[('country.no_api_key', 0)])
 
     def test_unknown_api_key(self):
-        result = self._make_geoip_query(api_key='unknown_key', status=400)
-        self._check_geoip_result(result, status=400)
-        self.check_db_calls(master=0, slave=1)
+        result = self._make_geoip_query(api_key='unknown_key', status=200)
+        self._check_geoip_result(result, status=200)
+        self.check_db_calls(master=0, slave=0)
         self.check_stats(
-            counter=['country.unknown_api_key'])
+            counter=[('country.unknown_api_key', 0)])
 
 
 class TestCountryErrors(AppTestCase, CountryBase):
--- ichnaea/service/country/views.py
+++ ichnaea/service/country/views.py
@@ -12,30 +12,36 @@ from ichnaea.service.locate import (
     map_data,
 )
 
+EMPTY_REQUEST = '{}'
+
 
 def configure_country(config):
     config.add_route('v1_country', '/v1/country')
     config.add_view(country_view, route_name='v1_country', renderer='json')
 
 
-@check_api_key('country')
+# Disable API key checks and logging for this API, for the initial wave
+# @check_api_key('country', error_on_invalidkey=False)
 def country_view(request):
+    if request.body == EMPTY_REQUEST:
+        # Optimize common case of geoip-only request
+        data = {}
+    else:
+        data, errors = preprocess_request(
+            request,
+            schema=GeoLocateSchema(),
+            response=JSONParseError,
+            accept_empty=True,
+        )
+        data = map_data(data)
 
-    data, errors = preprocess_request(
-        request,
-        schema=GeoLocateSchema(),
-        response=JSONParseError,
-        accept_empty=True,
-    )
-
-    data = map_data(data)
     session = request.db_slave_session
     result = search_all_sources(
         session, 'country', data,
         client_addr=request.client_addr,
         geoip_db=request.registry.geoip_db,
-        api_key_log=getattr(request, 'api_key_log', False),
-        api_key_name=getattr(request, 'api_key_name', None),
+        api_key_log=False,
+        api_key_name=None,
         result_type='country')
 
     if not result:
