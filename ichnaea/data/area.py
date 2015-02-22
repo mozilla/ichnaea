1d1eed55d8f8b2039e277c5d5f540d47e503b660
Hanno Schlichting <hanno@hannosch.eu>
2015-02-22 19:12:44 +0100

    Break out `scan_lacs/update_lac` related logic into separate module.

---
+++ ichnaea/data/area.py
@@ -0,0 +1,112 @@
+from ichnaea.data.station import (
+    dequeue_lacs,
+    UPDATE_KEY,
+)
+from ichnaea.geocalc import centroid, range_to_points
+from ichnaea.models import (
+    CELL_MODEL_KEYS,
+    CellArea,
+)
+from ichnaea import util
+
+
+class CellAreaUpdater(object):
+
+    def __init__(self, task, session,
+                 cell_model_key='cell',
+                 cell_area_model_key='cell_area'):
+        self.task = task
+        self.session = session
+        self.cell_model_key = cell_model_key
+        self.cell_model = CELL_MODEL_KEYS[cell_model_key]
+        self.cell_area_model_key = cell_area_model_key
+        self.cell_area_model = CELL_MODEL_KEYS[cell_area_model_key]
+        self.redis_client = task.app.redis_client
+        self.stats_client = task.stats_client
+        self.utcnow = util.utcnow()
+
+    def scan(self, update_task, batch=100):
+        redis_areas = dequeue_lacs(
+            self.redis_client, UPDATE_KEY['cell_lac'], batch=batch)
+        areas = set([CellArea.to_hashkey(area) for area in redis_areas])
+
+        for area in areas:
+            update_task.delay(
+                area.radio,
+                area.mcc,
+                area.mnc,
+                area.lac,
+                cell_model_key=self.cell_model_key,
+                cell_area_model_key=self.cell_area_model_key)
+        return len(areas)
+
+    def update(self, radio, mcc, mnc, lac):
+        # Select all cells in this area and derive a bounding box for them
+        cell_query = (self.session.query(self.cell_model)
+                                  .filter(self.cell_model.radio == radio)
+                                  .filter(self.cell_model.mcc == mcc)
+                                  .filter(self.cell_model.mnc == mnc)
+                                  .filter(self.cell_model.lac == lac)
+                                  .filter(self.cell_model.lat.isnot(None))
+                                  .filter(self.cell_model.lon.isnot(None)))
+
+        cells = cell_query.all()
+
+        area_query = (self.session.query(self.cell_area_model)
+                                  .filter(self.cell_area_model.radio == radio)
+                                  .filter(self.cell_area_model.mcc == mcc)
+                                  .filter(self.cell_area_model.mnc == mnc)
+                                  .filter(self.cell_area_model.lac == lac))
+
+        if len(cells) == 0:
+            # If there are no more underlying cells, delete the area entry
+            area_query.delete()
+        else:
+            # Otherwise update the area entry based on all the cells
+            area = area_query.first()
+
+            points = [(c.lat, c.lon) for c in cells]
+            min_lat = min([c.min_lat for c in cells])
+            min_lon = min([c.min_lon for c in cells])
+            max_lat = max([c.max_lat for c in cells])
+            max_lon = max([c.max_lon for c in cells])
+
+            bbox_points = [(min_lat, min_lon),
+                           (min_lat, max_lon),
+                           (max_lat, min_lon),
+                           (max_lat, max_lon)]
+
+            ctr = centroid(points)
+            rng = range_to_points(ctr, bbox_points)
+
+            # Switch units back to meters
+            ctr_lat = ctr[0]
+            ctr_lon = ctr[1]
+            rng = int(round(rng * 1000.0))
+
+            # Now create or update the area
+            num_cells = len(cells)
+            avg_cell_range = int(sum(
+                [cell.range for cell in cells]) / float(num_cells))
+            if area is None:
+                area = self.cell_area_model(
+                    created=self.utcnow,
+                    modified=self.utcnow,
+                    radio=radio,
+                    mcc=mcc,
+                    mnc=mnc,
+                    lac=lac,
+                    lat=ctr_lat,
+                    lon=ctr_lon,
+                    range=rng,
+                    avg_cell_range=avg_cell_range,
+                    num_cells=num_cells,
+                )
+                self.session.add(area)
+            else:
+                area.modified = self.utcnow
+                area.lat = ctr_lat
+                area.lon = ctr_lon
+                area.range = rng
+                area.avg_cell_range = avg_cell_range
+                area.num_cells = num_cells
--- ichnaea/data/tasks.py
+++ ichnaea/data/tasks.py
@@ -1,5 +1,6 @@
 from ichnaea.async.task import DatabaseTask
 from ichnaea.customjson import kombu_loads
+from ichnaea.data.area import CellAreaUpdater
 from ichnaea.data.observation import (
     CellObservationQueue,
     WifiObservationQueue,
@@ -7,19 +8,15 @@ from ichnaea.data.observation import (
 from ichnaea.data.report import ReportQueue
 from ichnaea.data.station import (
     enqueue_lacs,
-    dequeue_lacs,
     CellStationUpdater,
     WifiStationUpdater,
     UPDATE_KEY,
 )
-from ichnaea.geocalc import centroid, range_to_points
 from ichnaea.models import (
     Cell,
-    CELL_MODEL_KEYS,
     CellArea,
     Wifi,
 )
-from ichnaea import util
 from ichnaea.worker import celery
 
 
@@ -132,104 +129,22 @@ def remove_wifi(self, wifi_keys):
 
 @celery.task(base=DatabaseTask, bind=True)
 def scan_lacs(self, batch=100):
-    """
-    Find cell LACs that have changed and update the bounding box.
-    This includes adding new LAC entries and removing them.
-    """
-    redis_client = self.app.redis_client
-    redis_lacs = dequeue_lacs(
-        redis_client, UPDATE_KEY['cell_lac'], batch=batch)
-    lacs = set([CellArea.to_hashkey(lac) for lac in redis_lacs])
-
-    for lac in lacs:
-        update_lac.delay(
-            lac.radio,
-            lac.mcc,
-            lac.mnc,
-            lac.lac,
-            cell_model_key='cell',
-            cell_area_model_key='cell_area')
-    return len(lacs)
+    updater = CellAreaUpdater(
+        self, None,
+        cell_model_key='cell',
+        cell_area_model_key='cell_area')
+    length = updater.scan(update_lac, batch=batch)
+    return length
 
 
 @celery.task(base=DatabaseTask, bind=True)
 def update_lac(self, radio, mcc, mnc, lac,
-               cell_model_key='cell', cell_area_model_key='cell_area'):
-    utcnow = util.utcnow()
+               cell_model_key='cell',
+               cell_area_model_key='cell_area'):
     with self.db_session() as session:
-        # Select all the cells in this LAC that aren't the virtual
-        # cell itself, and derive a bounding box for them.
-
-        cell_model = CELL_MODEL_KEYS[cell_model_key]
-        cell_query = (session.query(cell_model)
-                             .filter(cell_model.radio == radio)
-                             .filter(cell_model.mcc == mcc)
-                             .filter(cell_model.mnc == mnc)
-                             .filter(cell_model.lac == lac)
-                             .filter(cell_model.lat.isnot(None))
-                             .filter(cell_model.lon.isnot(None)))
-
-        cells = cell_query.all()
-
-        cell_area_model = CELL_MODEL_KEYS[cell_area_model_key]
-        lac_query = (session.query(cell_area_model)
-                            .filter(cell_area_model.radio == radio)
-                            .filter(cell_area_model.mcc == mcc)
-                            .filter(cell_area_model.mnc == mnc)
-                            .filter(cell_area_model.lac == lac))
-
-        if len(cells) == 0:
-            # If there are no more underlying cells, delete the lac entry
-            lac_query.delete()
-        else:
-            # Otherwise update the lac entry based on all the cells
-            lac_obj = lac_query.first()
-
-            points = [(c.lat, c.lon) for c in cells]
-            min_lat = min([c.min_lat for c in cells])
-            min_lon = min([c.min_lon for c in cells])
-            max_lat = max([c.max_lat for c in cells])
-            max_lon = max([c.max_lon for c in cells])
-
-            bbox_points = [(min_lat, min_lon),
-                           (min_lat, max_lon),
-                           (max_lat, min_lon),
-                           (max_lat, max_lon)]
-
-            ctr = centroid(points)
-            rng = range_to_points(ctr, bbox_points)
-
-            # Switch units back to DB preferred centimicrodegres angle
-            # and meters distance.
-            ctr_lat = ctr[0]
-            ctr_lon = ctr[1]
-            rng = int(round(rng * 1000.0))
-
-            # Now create or update the LAC virtual cell
-            num_cells = len(cells)
-            avg_cell_range = int(sum(
-                [cell.range for cell in cells])/float(num_cells))
-            if lac_obj is None:
-                lac_obj = cell_area_model(
-                    created=utcnow,
-                    modified=utcnow,
-                    radio=radio,
-                    mcc=mcc,
-                    mnc=mnc,
-                    lac=lac,
-                    lat=ctr_lat,
-                    lon=ctr_lon,
-                    range=rng,
-                    avg_cell_range=avg_cell_range,
-                    num_cells=num_cells,
-                )
-                session.add(lac_obj)
-            else:
-                lac_obj.modified = utcnow
-                lac_obj.lat = ctr_lat
-                lac_obj.lon = ctr_lon
-                lac_obj.range = rng
-                lac_obj.avg_cell_range = avg_cell_range
-                lac_obj.num_cells = num_cells
-
+        updater = CellAreaUpdater(
+            self, session,
+            cell_model_key=cell_model_key,
+            cell_area_model_key=cell_area_model_key)
+        updater.update(radio, mcc, mnc, lac)
         session.commit()
