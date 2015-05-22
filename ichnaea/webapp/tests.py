c0f959446731b8ce2677c56afd5456c2e047cabb
Hanno Schlichting <hanno@hannosch.eu>
2015-05-22 15:21:38 +0200

    Change the webapp tests to not interfere with instance level connections.

--- ichnaea/webapp/tests.py
+++ ichnaea/webapp/tests.py
@@ -11,7 +11,7 @@ from ichnaea.tests.base import (
 
 class TestApp(RedisIsolation, DBTestCase):
 
-    def test_db_hooks(self):
+    def test_db_config(self):
         app_config = DummyConfig({'ichnaea': {
             'db_master': SQLURI,
             'db_slave': SQLURI,
@@ -21,28 +21,40 @@ class TestApp(RedisIsolation, DBTestCase):
                         _redis_client=self.redis_client,
                         _stats_client=self.stats_client,
                         )
-        self.db_rw = app.app.registry.db_rw
-        self.db_ro = app.app.registry.db_ro
-        app.get('/stats_wifi.json', status=200)
+        db_rw = app.app.registry.db_rw
+        db_ro = app.app.registry.db_ro
+        # the configured databases are working
+        try:
+            self.assertTrue(db_rw.ping())
+            self.assertTrue(db_ro.ping())
+        finally:
+            # clean up the new db engine's _make_app created
+            db_rw.engine.pool.dispose()
+            db_ro.engine.pool.dispose()
 
-    def test_db_config(self):
-        self.db_rw = _make_db()
-        self.db_ro = _make_db()
-        app = _make_app(_db_rw=self.db_rw,
-                        _db_ro=self.db_ro,
+    def test_db_hooks(self):
+        db_rw = _make_db()
+        db_ro = _make_db()
+        app = _make_app(_db_rw=db_rw,
+                        _db_ro=db_ro,
                         _raven_client=self.raven_client,
                         _redis_client=self.redis_client,
                         _stats_client=self.stats_client,
                         )
-        app.get('/stats_wifi.json', status=200)
+        # check that our _db hooks are passed through
+        self.assertTrue(app.app.registry.db_rw is db_rw)
+        self.assertTrue(app.app.registry.db_ro is db_ro)
 
     def test_redis_config(self):
         app_config = DummyConfig({'ichnaea': {
-            'db_master': SQLURI,
-            'db_slave': SQLURI,
             'redis_url': REDIS_URI,
         }})
         app = _make_app(app_config=app_config,
+                        _db_rw=self.db_rw,
+                        _db_ro=self.db_ro,
                         _raven_client=self.raven_client,
                         _stats_client=self.stats_client)
-        self.assertTrue(app.app.registry.redis_client is not None)
+        redis_client = app.app.registry.redis_client
+        self.assertTrue(redis_client is not None)
+        self.assertEqual(
+            redis_client.connection_pool.connection_kwargs['db'], 1)
