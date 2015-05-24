11db9f9a1593534e4c41d25ca5b5b3f2703314cf
Hanno Schlichting <hanno@hannosch.eu>
2015-05-24 17:13:01 +0200

    Make flake8 happy with alembic/docs directories.

--- alembic/versions/3b8d52a9eac4_tinyint_enum_columns.py
+++ alembic/versions/3b8d52a9eac4_tinyint_enum_columns.py
@@ -3,15 +3,14 @@
 Revision ID: 3b8d52a9eac4
 Revises: 10542c592089
 Create Date: 2015-02-16 15:42:55.185999
-
 """
+
+from alembic import op
+import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '3b8d52a9eac4'
 down_revision = '10542c592089'
-
-from alembic import op
-import sqlalchemy as sa
 
 CHANGES = [
     ('measure_block', 'measure_type'),
@@ -27,6 +26,7 @@ def upgrade():
 
 
 def downgrade():
-    stmt = "ALTER TABLE {table} CHANGE COLUMN `{column}` `{column}` SMALLINT(6)"
+    stmt = ("ALTER TABLE {table} "
+            "CHANGE COLUMN `{column}` `{column}` SMALLINT(6)")
     for table, column in CHANGES:
         op.execute(sa.text(stmt.format(table=table, column=column)))
