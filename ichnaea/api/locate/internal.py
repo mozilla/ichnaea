9fa1dafdd6c920a1c21daa9a1b39a4c6504ef5f4
Hanno Schlichting <hanno@hannosch.eu>
2016-01-19 11:11:35 +0100

    Remove code duplication in internal module.

--- ichnaea/api/locate/internal.py
+++ ichnaea/api/locate/internal.py
@@ -15,15 +15,14 @@ from ichnaea.api.locate.wifi import (
 )
 
 
-class InternalRegionSource(CellRegionMixin,
-                           WifiRegionMixin, RegionSource):
-    """A region source based on our own crowd-sourced internal data."""
+class BaseInternalSource(object):
+    """A source based on our own crowd-sourced internal data."""
 
     fallback_field = None  #:
     source = DataSource.internal  #:
 
     def should_search(self, query, results):
-        if not super(InternalRegionSource, self).should_search(
+        if not super(BaseInternalSource, self).should_search(
                 query, results):  # pragma: no cover
             return False
         if not (self.should_search_cell(query, results) or
@@ -45,31 +44,15 @@ class InternalRegionSource(CellRegionMixin,
         return results
 
 
-class InternalPositionSource(CellPositionMixin,
-                             WifiPositionMixin, PositionSource):
+class InternalPositionSource(BaseInternalSource,
+                             CellPositionMixin,
+                             WifiPositionMixin,
+                             PositionSource):
     """A position source based on our own crowd-sourced internal data."""
 
-    fallback_field = None  #:
-    source = DataSource.internal  #:
 
-    def should_search(self, query, results):
-        if not super(InternalPositionSource, self).should_search(
-                query, results):  # pragma: no cover
-            return False
-        if not (self.should_search_cell(query, results) or
-                self.should_search_wifi(query, results)):
-            return False
-        return True
-
-    def search(self, query):
-        results = self.result_type().new_list()
-
-        for should, search in (
-                (self.should_search_wifi, self.search_wifi),
-                (self.should_search_cell, self.search_cell)):
-
-            if should(query, results):
-                results.add(search(query))
-
-        query.emit_source_stats(self.source, results)
-        return results
+class InternalRegionSource(BaseInternalSource,
+                           CellRegionMixin,
+                           WifiRegionMixin,
+                           RegionSource):
+    """A region source based on our own crowd-sourced internal data."""
