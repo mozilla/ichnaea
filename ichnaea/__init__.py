a84315bf9feddffd320c8e7ac95682b769091394
John Whitlock <jwhitlock@mozilla.com>
2019-09-23 10:03:19 -0500

    Remove manual call to shapely.speedups.enable()
    Shapely 1.6 now automatically applies speedups if the GEOS libraries and
    C tools are available at install. speedups.enable() is no longer needed,
    and speedups.disable() will use the Python-native algorithms.

--- ichnaea/__init__.py
+++ ichnaea/__init__.py
@@ -1,12 +1,5 @@
 # Disable deprecation warnings in production mode
 import warnings
 
-from shapely import speedups
-
 warnings.simplefilter("ignore", DeprecationWarning)
 warnings.simplefilter("ignore", PendingDeprecationWarning)
-
-# Enable shapely speedups
-if speedups.available:
-    speedups.enable()
-del speedups
