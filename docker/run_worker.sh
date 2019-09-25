51737423f1c47966f860b1e29fe74b37710a8055
John Whitlock <jwhitlock@mozilla.com>
2019-09-25 14:57:05 -0500

    Update to celery 4.0.2
    Requirement updates:
    * celery 3.1.26.post2 → celery 4.0.2: Many breaking changes
    
    Compatible dependency upgrades:
    * amqp 1.4.9 → 2.5.1: Drop Python 2.6 and 3.4, other fixes
    * billiard 3.3.0.23 → 3.5.0.5: Drop Python 2.6, other fixes
    * kombu 3.0.37 → 4.1.0: Requires amqp 2.x, drops anyjson, drops
      unsupported transports like Django ORM, add new transports like SQS.
      JSON serializer calls __json__ method if available.
    * vine 1.3.0: New requirement of amqp 2.1.0
    
    Convert code to Celery 4:
    * Use lowercase settings names
    * Use --loglevel=INFO to see tasks as they fly by
    * Drop BaseTask attributes that are the default or are loaded from the
      configuration
    * Adopt other BBB notes

--- docker/run_worker.sh
+++ docker/run_worker.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-
-# BBB: Celery 4
-# celery -A ichnaea.taskapp.app:celery_app worker
-
 celery -A ichnaea.taskapp.app:celery_app worker \
-    -Ofair --without-mingle --without-gossip --no-execv
+    --loglevel=INFO
