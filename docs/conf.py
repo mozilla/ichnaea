5cb8d2b92949ad990946bc0e7d2dcd3a11f999c4
Dustin J. Mitchell <dustin@mozilla.com>
2020-09-08 20:40:08 +0000

    run lint over specific directories, fix black issues

--- docs/conf.py
+++ docs/conf.py
@@ -11,44 +11,45 @@ sys.path.append(REPO_DIR)
 
 
 # Fake the shapely module so things will import
-sys.modules['shapely'] = mock.MagicMock()
+sys.modules["shapely"] = mock.MagicMock()
 
 
-project = 'Ichnaea'
-copyright = '2013-2020, Mozilla'
+project = "Ichnaea"
+copyright = "2013-2020, Mozilla"
 
 # The short X.Y version.
-version = '2.0'
+version = "2.0"
 # The full version, including alpha/beta/rc tags.
-release = '2.0'
+release = "2.0"
 
-autoclass_content = 'class'
-exclude_patterns = ['_build', '.DS_Store', 'Thumbs.db']
+autoclass_content = "class"
+exclude_patterns = ["_build", ".DS_Store", "Thumbs.db"]
 html_static_path = []
-modindex_common_prefix = ['ichnaea.']
-pygments_style = 'sphinx'
-source_suffix = '.rst'
-templates_path = ['_templates']
+modindex_common_prefix = ["ichnaea."]
+pygments_style = "sphinx"
+source_suffix = ".rst"
+templates_path = ["_templates"]
 
 # Use default theme if we are in ReadTheDocs
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
+on_rtd = os.environ.get("READTHEDOCS") == "True"
 if on_rtd:
-    html_theme = 'default'
+    html_theme = "default"
 else:
     import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
+
+    html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 extensions = [
-    'sphinx.ext.linkcode',
-    'everett.sphinxext',
+    "sphinx.ext.linkcode",
+    "everett.sphinxext",
 ]
 
 
 def linkcode_resolve(domain, info):
-    if domain != 'py':
+    if domain != "py":
         return None
-    if not info['module']:
+    if not info["module"]:
         return None
-    filename = info['module'].replace('.', '/')
+    filename = info["module"].replace(".", "/")
     return "https://github.com/mozilla/ichnaea/tree/main/%s.py" % filename
